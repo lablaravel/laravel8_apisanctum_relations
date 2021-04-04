8a04572f84f73c8cfef461e3b1c50957b3f734ed
pwcodigo <pwcodigo@hotmail.com>
2021-04-04 01:41:59 -0300

    feat: adicionado campo photo e também cpf e cnpj

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');            
+            $table->string('password');
+            $table->string('photo');
+            $table->string('cpf',11)->nullable();
+            $table->string('cnpj',14)->nullable();            
             $table->rememberToken();
             $table->timestamps();
         });
